54809340dccca03580ad68037a0b2bfe2430006c
Vumba798 <alexandrov32649@gmail.com>
2021-11-01 14:11:24 +0300

    added work with files and printing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
-suspend fun main() {
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
+suspend fun main(): Unit = coroutineScope {
     try {
-        val analyzes = RateAnalyzer().getAnalyzeResult()
+        val analyzer = RateAnalyzer()
+        val analyzes = analyzer.getAnalyzeResult()
         println("growing are:")
         for (element in analyzes.growing) {
             println(element.toString())
@@ -8,6 +12,9 @@ suspend fun main() {
         println("\nmin = ${analyzes.min}%")
         println("max = ${analyzes.max}%")
         println("avg = ${analyzes.avg}%")
+        analyzer.saveToFile("savedResults.json")
+        val analyzesFromFile = readFromFileAnalyzes("savedResults.json")
+        println(analyzes == analyzesFromFile)
     } catch (exception: Exception) {
         println(exception.message)
     }
--- src/main/kotlin/RateAnalyzer.kt
+++ src/main/kotlin/RateAnalyzer.kt
@@ -1,6 +1,10 @@
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
+import com.google.gson.annotations.SerializedName
 import kotlinx.coroutines.*
+import java.io.File
 import java.net.HttpURLConnection
 import java.net.URL
 import java.time.LocalDate
@@ -14,19 +18,35 @@ data class Rate(
 )
 
 data class AnalyzeResult(
-    val rates: List<Rate>,
-    val growing: List<Rate>,
+    @SerializedName("min")
     val min: Float,
+    @SerializedName("max")
     val max: Float,
-    val avg: Float
+    @SerializedName("avg")
+    val avg: Float,
+    @SerializedName("growing")
+    val growing: List<Rate>,
+    @SerializedName("rates")
+    val rates: List<Rate>
 )
 
+suspend fun readFromFileAnalyzes(path: String): AnalyzeResult = coroutineScope {
+    val deferredAnalyzes = async(Dispatchers.IO) {
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        val jsonString = File(path).bufferedReader().use {
+            it.readText()
+        }
+        return@async gson.fromJson(jsonString, AnalyzeResult::class.java)
+    }
+    return@coroutineScope deferredAnalyzes.await()
+}
 class RateAnalyzer {
     private var rates = mutableListOf<Rate>()
     private var growing: List<Rate> = mutableListOf()
     private var min: Float = 0F
     private var max: Float = 0F
     private var avg: Float = 0F
+    private var jsonString = ""
 
     private fun getDateMonthAgo() = LocalDate.now(ZoneId.systemDefault()).minusMonths(1)
 
@@ -44,7 +64,8 @@ class RateAnalyzer {
         }
         val deferredMonthAgo: Deferred<JsonObject> = async(Dispatchers.IO) {
             val dateMonthAgo = getDateMonthAgo().toString()
-            val url = URL("https://openexchangerates.org/api/historical/$dateMonthAgo.json?app_id=d1290d44145b4d62b6760da7db9446e8")
+            val url =
+                URL("https://openexchangerates.org/api/historical/$dateMonthAgo.json?app_id=d1290d44145b4d62b6760da7db9446e8")
             val apiHistoricalString = with(url.openConnection() as HttpURLConnection) {
                 requestMethod = "GET"
                 inputStream.bufferedReader().use {
@@ -61,12 +82,12 @@ class RateAnalyzer {
             rates.add(Rate(charCode, monthAgoRates[charCode].asFloat, latestRates[charCode].asFloat))
         }
         updateVars()
-        return@coroutineScope AnalyzeResult(rates, growing, min, max, avg)
+        return@coroutineScope AnalyzeResult(min, max, avg, growing, rates)
     }
 
     private fun updateVars() {
         growing = rates
-            .filter { it.enhancement > 0}
+            .filter { it.enhancement > 0 }
             .map { it }
         min = rates
             .minOf { it.enhancement }
@@ -77,6 +98,24 @@ class RateAnalyzer {
                 it.enhancement.toDouble()
             }.toFloat()
         avg /= rates.size
+
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        jsonString = gson.toJson(AnalyzeResult(min, max, avg, growing, rates))
     }
 
+    fun getJson(): String {
+        return jsonString
+    }
+    suspend fun saveToFile(path: String) = coroutineScope {
+        launch(Dispatchers.IO) {
+            File(path).writeText(jsonString)
+        }
+    }
+    suspend fun print() = coroutineScope {
+        launch(Dispatchers.Main) {
+            println(jsonString)
+        }
+    }
 }
+
+
